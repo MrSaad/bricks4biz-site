23407efd4f984616de360c14213721183d585645
Saad Ahmed <saadem3000@gmail.com>
2018-05-12 05:03:56 -0700

    Add some content

--- public/js/script-index.js
+++ public/js/script-index.js
@@ -5,14 +5,14 @@ $(() => {
 	$('#main-nav').addClass('active-nav');
 
 	var topoffset=0
-	var wheight = $(window).height(); //height of window
+	var wheight = $(window).height()*0.9; //height of window
 
 	//set height of main page 
 	$('.page#main').css('height', wheight);
 
 	//adjust height of main page on window resize
 	$(window).resize(function() {
-		wheight = $(window).height(); //get the height of the window
+		wheight = $(window).height()*0.9; //get the height of the window
 		$('.page#main').css('height', wheight); //set to window tallness  
 	});
 
