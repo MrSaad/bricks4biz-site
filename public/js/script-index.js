868525c1655c18666482e52ef7702248825632af
Saad Ahmed <saadem3000@gmail.com>
2018-05-19 16:47:40 -0700

    Fix FAQ and other padding issues

--- public/js/script-index.js
+++ public/js/script-index.js
@@ -4,15 +4,18 @@ $(() => {
 	//set Main nav item to highlighted
 	$('#main-nav').addClass('active-nav');
 
+	// percentage of full page the main page covers
+	var pageCover = 0.95;
+
 	var topoffset=0
-	var wheight = $(window).height()*0.9; //height of window
+	var wheight = $(window).height()*pageCover; //height of window
 
 	//set height of main page 
 	$('.page#main').css('height', wheight);
 
 	//adjust height of main page on window resize
 	$(window).resize(function() {
-		wheight = $(window).height()*0.9; //get the height of the window
+		wheight = $(window).height()*pageCover; //get the height of the window
 		$('.page#main').css('height', wheight); //set to window tallness  
 	});
 
