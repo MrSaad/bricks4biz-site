f569e18b95f71963ff78f10cdd7e85e42d1d6fc9
Saad Ahmed <saadem3000@gmail.com>
2019-03-21 22:31:23 -0700

    Add email functionality

---
+++ models/email-template.js
@@ -0,0 +1,22 @@
+template = "<body> \
+			<strong>Full Name: </strong> {{fullname}} \
+			<br /><br/> \
+			\
+			<strong>Email: </strong> {{eml}} \
+			<br /><br/> \
+			\
+			<strong>Organization: </strong> {{org}} \
+			<br /><br/> \
+			\
+			<strong>Phone Number: </strong> {{phone}} \
+			<br /><br/> \
+			\
+			<strong>City, State, Zip: </strong> {{loc}} \
+			<br /><br/> \
+			\
+			<strong>Message: </strong> {{msg}} \
+			<br /><br/> \
+			\
+		</body>";
+
+module.exports = template;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 var _ = require('lodash');
 var express = require('express');
+const multiparty = require('multiparty');
 var locations = require('../models/locations.js');
 var iLocations = require('../models/international-locations.js');
+const sgMail = require('@sendgrid/mail');
+const demo_template = require('../models/email-template.js');
+
+let sendToEmail = process.env.SEND_TO_EMAIL;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setSubstitutionWrappers('{{', '}}');
 var router = express.Router();
 
 /* GET Home page. */
@@ -49,6 +56,65 @@ router.get('/ilocdata/:country', function(req, res, next) {
 
 /* POST Email Submission */
 router.post('/requestcontact', function(req, res, next){
+	const form = new multiparty.Form();
+
+	// splits form-data into keys, fields and files, where values are arrays of associated data
+	form.parse(req, function(err, fields, files) {
+
+		if (err) {
+			console.log("invalid request: " + err.message);
+			return;
+		}
+
+		// Extract fields from form
+		try {
+			const formData = {
+				name : 			fields.name[0],
+				org : 			fields.org[0],
+				email : 		fields.email[0],
+				phone : 		fields.phone[0],
+				location : 	fields.location[0],
+				msg : 			fields.msg[0],
+			};
+
+			// Create SendGrid message and send email to HyperCare
+			const msg = {
+				'personalizations': [{
+					'to': [{
+						"email": sendToEmail,
+						"name": "Testing Stuff"
+					}],
+					'subject': 'Contact Request from {{fullname}} @ {{org}}',
+					'substitutions': {
+						'fullname': formData.name,
+						'org': formData.org,
+						'eml': formData.email,
+						'phone': formData.phone,
+						'loc': formData.location,
+						'msg': formData.msg
+					}
+				}],
+				'from': {
+					"email": formData.email
+				},
+				'reply_to': {
+					'email': formData.email,
+					'name': formData.name
+				},
+				'html': demo_template
+			};
+			sgMail.send(msg);
+
+			console.log(formData);
+
+		} catch (err) {
+			console.log("error: \n" + err);
+			return;
+		} finally {
+			// do nothing
+		}
+	});
+
 	res.render('contact', {title: "Bricks4Biz - Contact", success: true});
 });
 
